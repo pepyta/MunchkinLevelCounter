38b4b3ee1ab71720a25256ff03d0f95034f2fff4
Gál Péter <pepyta118@gmail.com>
2018-10-03 17:16:20 +0200

    🎲 Roll a Dice

--- app/src/main/java/com/pepyta/munchkinlevelcounter/MainActivity.java
+++ app/src/main/java/com/pepyta/munchkinlevelcounter/MainActivity.java
@@ -1,10 +1,15 @@
 package com.pepyta.munchkinlevelcounter;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity implements SensorListener{
     int gear = 0;
     int level = 1;
     @Override
@@ -50,6 +55,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void roll(android.view.View view){
+        updateRNG(rollDice());
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Roll");
+        alert.setMessage(Integer.toString(rollDice()));
+        alert.show();
+
+    }
+    public int rollDice(){
+        int dobas = (int)(Math.random()*6+1);
+        return dobas;
+    }
+
     private void updateLevel() {
         TextView displayInteger = (TextView)findViewById(R.id.level);
         displayInteger.setText(Integer.toString(level));
@@ -61,7 +82,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateOverall() {
-        TextView displayInteger = (TextView)findViewById(R.id.overall);
+        TextView displayInteger = (TextView)findViewById(R.id.total);
         displayInteger.setText(Integer.toString(level + gear));
     }
+
+    private void updateRNG(int dobas) {
+        TextView displayInteger = (TextView)findViewById(R.id.rng);
+        displayInteger.setText(Integer.toString(dobas)); // "dasdsasdajdsoajdosajoasj" 132 => "132"
+    }
+
+
+    SensorManager sensorMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
+sensorMgr.registerListener(this,
+    SensorManager.SENSOR_ACCELEROMETER,
+    SensorManager.SENSOR_DELAY_GAME);
+    public void onSensorChanged(int sensor, float[] values) {
+        if (sensor == SensorManager.SENSOR_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                x = values[SensorManager.DATA_X];
+                y = values[SensorManager.DATA_Y];
+                z = values[SensorManager.DATA_Z];
+
+                float speed = Math.abs(x+y+z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    Log.d("sensor", "shake detected w/ speed: " + speed);
+                    Toast.makeText(this, "shake detected w/ speed: " + speed, Toast.LENGTH_SHORT).show();
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    private static final int SHAKE_THRESHOLD = 800;
 }
